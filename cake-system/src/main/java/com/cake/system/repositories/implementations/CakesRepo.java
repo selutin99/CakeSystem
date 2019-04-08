eef4acbce793945f1cffe995124063309209ac78
Alexander <cool.selutin99@yandex.ru>
2019-04-08 21:25:12 +0400

    cakes repo

--- cake-system/src/main/java/com/cake/system/repositories/implementations/CakesRepo.java
+++ cake-system/src/main/java/com/cake/system/repositories/implementations/CakesRepo.java
@@ -1,4 +1,14 @@
 package com.cake.system.repositories.implementations;
 
-public class CakesRepo {
+import com.cake.system.entity.Cakes;
+import com.cake.system.repositories.ImplUnitOfWork;
+import com.cake.system.storages.Database;
+
+import java.util.List;
+import java.util.Map;
+
+public class CakesRepo extends ImplUnitOfWork<Cakes> {
+    public CakesRepo(Map<String, List<Cakes>> context, Database database) {
+        super(context, database);
+    }
 }
